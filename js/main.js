fec9998c81aa518b9ca16ff68b0eca8ef236219a
bassiebangura <devbassiebangura@gmail.com>
2019-04-26 17:20:50 -0400

    Create fetchData function.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-fetch('https://randomuser.me/api/?results=1')
-    .then(response => response.json())
-    .then(data => console.log(data));
+const addData = (data) => {
+    let firstName = data.results[0].name.first; 
+    let lastName = data.results[0].name.last;
+    let city = data.results[1].location.city;
+    let emailAddress = data.results[2].email;
+    console.log(`${firstName} ${lastName} ${emailAddress} ${city}`)
+    }
+
+const fetchData = (url) => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            addData(data)
+        });
+}
+
+fetchData('https://randomuser.me/api/?results=12');
+   
